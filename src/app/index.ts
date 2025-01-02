86e62439344e96a9bda942e57897777afd986a7c
Clover You <50982977+Clover-You@users.noreply.github.com>
2025-01-02 16:26:39 +0800

    Update index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,13 @@
+import { koConvert } from './ko'
 import { xiaotieConvert } from './xiaotie'
 
 export const appList = [
   {
     excutor: xiaotieConvert,
     name: 'xiaotie'
+  },
+  {
+    excutor: koConvert,
+    name: 'ko'
   }
 ] as const
