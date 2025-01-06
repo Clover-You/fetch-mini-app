e9dfe1419027aa7b3605da312e5e28bbc0b9b939
Clover You <2621869236@qq.com>
2025-01-06 15:42:47 +0800

    refactor: sequential execution

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import { appList } from './app'
 
 (async () => {
 
-  const records = await Promise.all(
-    appList.map(async ({ excutor, name }) => {
-      const importResult = await import(`../data/${name}.json`)
-      const cache: Record<string, StoreDetail> = importResult.default ?? name
-      return [name, await excutor(cache)] as const
-    })
-  )
+  const records: [string, Record<string, StoreDetail>][] = []
+
+  for (const { excutor, name } of appList) {
+    const importResult = await import(`../data/${name}.json`)
+    const cache: Record<string, StoreDetail> = importResult.default ?? name
+
+    records.push([name, await excutor(cache)] as const)
+  }
 
   for (const [name, data] of records) {
     fs.writeFile(`data/${name}.json`, JSON.stringify(data, null, 2))
